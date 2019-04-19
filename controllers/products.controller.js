dccdcc814b1577a390116a490f7a171419b0f302
Kirill <Starkeiman@yandex.ru>
2019-04-19 18:42:08 +0300

    Implements based CRUD operations

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,13 +1,59 @@
 const productModel = require("../models/product");
-const productService = require("../services/products.service");
-
+const types = require("mongoose").Types;
 class ProductsController {
-
     static getProducts(req, res) {
         productModel.find((err, data) => {
-            if (err) console.error(err);
-            res.send(data);
+            ProductsController.sendData(res, err, data);
         });
+    }
+
+    //TODO: Should be tested
+    static updateProduct(req, res) {
+        let product = req.body;
+        productModel.findOneAndUpdate({
+            _id: product._id
+        }, product, {
+            useFindAndModify: false
+        }, (err, data) => {
+            ProductsController.sendData(res, err, data);
+        });
+    }
+
+    static getProductById(req, res) {
+
+        let _id = req.params._id;
+        productModel.findOne({
+            _id
+        }, (err, data) => {
+            ProductsController.sendData(res, err, data);
+        });
+    }
+
+    static addProduct(req, res) {
+        let product = new productModel(req.body);
+        product._id = new types.ObjectId();
+        product.save();
+        res.status(200).send({
+            message: "successful"
+        });
+    }
+
+    static deleteProduct(req, res) {
+        let _id = req.query._id;
+        productModel.deleteOne({
+            _id
+        }, (err) => {
+            if (err) res.send(err);
+            else
+                res.status(200).send({
+                    message: "successful"
+                });
+        })
+    }
+
+    static sendData(res, err, data) {
+        if (err) res.status(404).send(err);
+        res.status(200).send(data);
     }
 
 }
