32e1e0224a1dc4a8134829bd22345139a47089d0
Kirill <Starkeiman@yandex.ru>
2019-04-19 18:44:34 +0300

    Implements controller in product router

--- routers/products.router.js
+++ routers/products.router.js
@@ -2,5 +2,10 @@ const router = require("express").Router();
 const productController = require("../controllers/products.controller");
 
 router.get("/products", productController.getProducts);
+router.get("/product/:_id", productController.getProductById);
+router.put("/product", productController.updateProduct);
+router.post("/product", productController.addProduct);
+router.delete("/product/:_id", productController.deleteProduct);
+
 
 module.exports = router;
