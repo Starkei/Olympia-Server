70656d571f5c0281a4ce143c77758b9d9fc48827
TimRois <andrei_k99@mail.ru>
2019-04-30 00:00:43 +0300

    Enable router in middleware

--- app.js
+++ app.js
@@ -11,9 +11,11 @@ const app = express();
 
 //middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: true
-}));
+  })
+);
 
 //process variables
 const port = process.env.PORT || 8080;
@@ -21,36 +23,41 @@ const password = process.env.DB_PASSWORD;
 const user = process.env.DB_USER;
 
 //database
-mongoose.connect(`mongodb+srv://${user}:${password}@cluster0-y8wr4.azure.mongodb.net/olympia?retryWrites=true`, {
+mongoose.connect(
+  `mongodb+srv://${user}:${password}@cluster0-y8wr4.azure.mongodb.net/olympia?retryWrites=true`,
+  {
     useNewUrlParser: true
-});
+  }
+);
 
 //routers
 const productRouter = require("./routers/products.router");
 const crowdfundingRouter = require("./routers/crowdfunding.router");
 const sportRouter = require("./routers/sport.router");
+const eventRouter = require("./routers/event.router");
 
 app.use("/", productRouter);
 app.use("/", crowdfundingRouter);
 app.use("/", sportRouter);
+app.use("/", eventRouter);
 
 const server = app.listen(port, () => {
-    console.log(`PID = ${pid}`);
+  console.log(`PID = ${pid}`);
 });
 
 process.on("SIGINT", () => {
-    mongoose.disconnect();
-    server.close();
+  mongoose.disconnect();
+  server.close();
 });
 
 process.on("SIGTERM", () => {
-    mongoose.disconnect();
-    server.close();
+  mongoose.disconnect();
+  server.close();
 });
 
 process.on("exit", () => {
-    mongoose.disconnect();
-    server.close();
+  mongoose.disconnect();
+  server.close();
 });
 
 module.exports = app;
