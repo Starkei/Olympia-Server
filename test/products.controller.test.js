af3361f292014efd48bc4f1af028457b4227e28b
Kirill <Starkeiman@yandex.ru>
2019-04-29 16:56:58 +0300

    Replaced test order for product controller

--- test/products.controller.test.js
+++ test/products.controller.test.js
@@ -6,6 +6,35 @@ chai.use(chaiHttp);
 chai.should();
 
 describe("Products", () => {
+    let postedProductId = "";
+    let postedProductData = {};
+
+    describe("POST /", () => {
+        it("should add product", done => {
+            let productData = {
+                type: ["Одежда"],
+                currency: "бел.руб",
+                description: "Шорты женские Smmash – лучший вариант для стильных девушек, подойдут для занятий фитнесом, кросс-фитом и просто для занятий в зале.",
+                firm: "Smash",
+                image: "http://bigsport.by/userfiles/shop/large_shop/21206_shorty-smmash-zhenskie-23021.jpeg",
+                price: 123,
+                title: "Шорты Smash (Женские) 23021",
+            };
+            chai
+                .request(app)
+                .post("/product")
+                .send(productData)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a("object");
+                    postedProductData = res.body;
+                    postedProductId = res.body._id;
+                    delete postedProductData._id;
+                    done();
+                });
+        });
+    });
+
     describe("GET /", () => {
         it("should get all students record", done => {
             chai
@@ -18,12 +47,11 @@ describe("Products", () => {
                 });
         });
 
-        //IMPORTANT: passed id should be exists
         it("should get product by id", done => {
-            let _id = "5cb5df405e8abe24a4733731";
+            console.log(postedProductId);
             chai
                 .request(app)
-                .get(`/product/${_id}`)
+                .get(`/product/${postedProductId}`)
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.body.should.be.a("object");
@@ -32,7 +60,7 @@ describe("Products", () => {
         });
 
         it("should not get product by id and return 404", done => {
-            let _id = "haha";
+            let _id = "empty";
             chai
                 .request(app)
                 .get(`/product/${_id}`)
@@ -42,65 +70,52 @@ describe("Products", () => {
                 });
         });
     });
+
     describe("PUT /", () => {
-        //IMPORTANT: passed id should be exists
         it("should update product by id", done => {
-            let product = {
-                _id: "5cb5ede0e9c7442284d80c24",
-                type: ["Одежда"],
-                currency: "бел.руб",
-                description: "Шорты женские Smmash – лучший вариант для стильных девушек, подойдут для занятий фитнесом, кросс-фитом и просто для занятий в зале.",
-                firm: "Sash",
-                image: "http://bigsport.by/userfiles/shop/large_shop/21206_shorty-smmash-zhenskie-23021.jpeg",
-                price: 123,
-                title: "Шорты Smash (Женские) 23021",
-            };
+            postedProductData.firm = "Sash";
             chai
                 .request(app)
-                .put(/product/)
-                .send(product)
+                .put(`/product/${postedProductId}`)
+                .send(postedProductData)
                 .end((err, res) => {
                     res.should.have.status(200);
+                    res.body.should.be.a("object");
+                    done();
+                });
+        });
+        it("should not update product by id and return 404", (done) => {
+            let _id = "empty";
+            chai
+                .request(app)
+                .get(`/product/${_id}`)
+                .send(postedProductData)
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    res.body.should.be.a("object");
                     done();
                 });
         });
     })
 
-    describe("POST /", () => {
-        //IMPORTANT: passed id should be exists
-        it("should add product", done => {
-            let product = {
-                type: ["Одежда"],
-                currency: "бел.руб",
-                description: "Шорты женские Smmash – лучший вариант для стильных девушек, подойдут для занятий фитнесом, кросс-фитом и просто для занятий в зале.",
-                firm: "Smash",
-                image: "http://bigsport.by/userfiles/shop/large_shop/21206_shorty-smmash-zhenskie-23021.jpeg",
-                price: 123,
-                title: "Шорты Smash (Женские) 23021",
-            };
-            chai
-                .request(app)
-                .post("/product")
-                .send(product)
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.have.property("message");
-                    res.body.message.should.equal("successful");
-                    done();
-                });
-        });
-    })
 
     describe("DELETE /", () => {
-        //IMPORTANT: passed id should be exists
         it("should delete product by id", done => {
-            let _id = "5cb5ede0e9c7442284d80c24";
+            chai
+                .request(app)
+                .delete(`/product/${postedProductId}`)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    done();
+                });
+        });
+        it("should not delete product by id and return 404", (done) => {
+            let _id = "empty";
             chai
                 .request(app)
                 .delete(`/product/${_id}`)
                 .end((err, res) => {
-                    res.body.should.have.property("message");
-                    res.body.message.should.equal("successful");
+                    res.should.have.status(404);
                     done();
                 });
         });
