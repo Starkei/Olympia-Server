91e953914b0106f5a2dad070a8dd065cc3d66f7b
Kirill <Starkeiman@yandex.ru>
2019-04-29 16:56:35 +0300

    Updated product router

--- routers/products.router.js
+++ routers/products.router.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
-const productController = require("../controllers/products.controller");
+const ProductController = require("../controllers/products.controller");
+const productController = new ProductController();
 
-router.get("/products", productController.getProducts);
-router.get("/product/:_id", productController.getProductById);
-router.put("/product", productController.updateProduct);
-router.post("/product", productController.addProduct);
-router.delete("/product/:_id", productController.deleteProduct);
-
+router.get("/products", productController.getProducts.bind(productController));
+router.get("/product/:_id", productController.getProductById.bind(productController));
+router.put("/product/:_id", productController.updateProduct.bind(productController));
+router.delete("/product/:_id", productController.deleteProduct.bind(productController));
+router.post("/product", productController.addProduct.bind(productController));
 
 module.exports = router;
