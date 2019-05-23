0a1aea29b3f88bcfef7924dd8bf05115d30c324f
Kirill <Starkeiman@yandex.ru>
2019-05-23 16:25:05 +0300

    Created adware controller.

---
+++ controllers/adware.controller.js
@@ -0,0 +1,32 @@
+const AdwareModel = require("../models/adware");
+const CRUDController = require("./CRUD.controller");
+
+class AdwareController extends CRUDController {
+
+    constructor() {
+        super(AdwareModel);
+    }
+
+    getAllAdware(req, res) {
+        super.getAll(req, res);
+    }
+
+    getAdwareById(req, res) {
+        super.getById(req, res);
+    }
+
+    updateAdware(req, res) {
+        super.update(req, res);
+    }
+
+    deleteAdware(req, res) {
+        super.delete(req, res);
+    }
+
+    addAdware(req, res) {
+        super.save(req, res);
+    }
+
+}
+
+module.exports = CrowdfundingController;
