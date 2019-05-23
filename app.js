cf46855f3d91e8b668c2b573eada04d96fb21aab
Kirill <Starkeiman@yandex.ru>
2019-05-23 16:30:10 +0300

    Enabled CORS headers.

--- app.js
+++ app.js
@@ -16,6 +16,13 @@ app.use(
     extended: true
   })
 );
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 //process variables
 const port = process.env.PORT || 8080;
@@ -24,8 +31,7 @@ const user = process.env.DB_USER;
 
 //database
 mongoose.connect(
-  `mongodb+srv://${user}:${password}@cluster0-y8wr4.azure.mongodb.net/olympia?retryWrites=true`,
-  {
+  `mongodb+srv://${user}:${password}@cluster0-y8wr4.azure.mongodb.net/olympia?retryWrites=true`, {
     useNewUrlParser: true
   }
 );
@@ -37,12 +43,14 @@ const userRouter = require("./routers/user.router");
 const newsRouter = require("./routers/news.router");
 const sportRouter = require("./routers/sport.router");
 const trainingRouter = require("./routers/training.router");
+const adwareRouter = require("./routers/adware.router");
 app.use("/", sportRouter);
 app.use("/", productRouter);
 app.use("/", crowdfundingRouter);
 app.use("/", userRouter);
 app.use("/", newsRouter);
 app.use("/", trainingRouter);
+app.use("/", adwareRouter);
 
 const server = app.listen(port, () => {
   console.log(`PID = ${pid}`);
